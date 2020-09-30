0390bd36351b231a69d716cce6e80e0326c9f49b
Rubemaster <ruben.rick@outlook.com>
2020-09-30 08:56:14 +0200

    Updates

--- src/Elements/BannerImage.js
+++ src/Elements/BannerImage.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
 export const BannerImage = () => (
-  <Container fluid style={{ backgroundColor: "red" }}>
+  <Container fluid style={{ backgroundColor: "blue" }}>
     nothing much
   </Container>
 );
